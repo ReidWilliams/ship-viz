249e412949215f4ed51005564e0c93ef4536bd43
Reid Williams <rwilliams@ideo.com>
2016-12-07 14:20:29 -0800

    initial commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -6,57 +6,40 @@ import s from './styles.css'
 import gs from './../../styles/grid.css'
 import Card from '../../components/Card/Card'
 
-class HomePage extends Component {
+const boundingBox = [
+  [37.78170504295941, -122.38460540771484], // sw - lower
+  [37.79934038764369, -122.32899312158202]  // ne - upper
+]
 
-  render() {
-    return (
-      <div className={gs.container}>
-        <div className={gs.line}>
-          <div className={s.cardContainer}>
-            <Card
-              target="/stupid-tree"
-              header="Idiotic Tree Keeps Trying To Plant Seeds On Sidewalk"
-              body="PORTLAND, OR—Pointing out that the total dipshit had dropped
-                dozens of acorns all along the length of pavement, sources
-                confirmed Thursday that a completely idiotic tree keeps trying
-                to plant seeds on the sidewalk. “Come on, you fucking moron, what
-                are you doing? That’s concrete"/>
-          </div>
-          <div className={s.cardContainer}>
-            <Card
-              target="/stupid-tree"
-              header="Idiotic Tree Keeps Trying To Plant Seeds On Sidewalk"
-              body="PORTLAND, OR—Pointing out that the total dipshit had dropped
-                dozens of acorns all along the length of pavement, sources
-                confirmed Thursday that a completely idiotic tree keeps trying
-                to plant seeds on the sidewalk. “Come on, you fucking moron, what
-                are you doing? That’s concrete"/>
-          </div>
-          <div className={s.cardContainer}>
-            <Card
-              target="/stupid-tree"
-              header="Idiotic Tree Keeps Trying To Plant Seeds On Sidewalk"
-              body="PORTLAND, OR—Pointing out that the total dipshit had dropped
-                dozens of acorns all along the length of pavement, sources
-                confirmed Thursday that a completely idiotic tree keeps trying
-                to plant seeds on the sidewalk. “Come on, you fucking moron, what
-                are you doing? That’s concrete"/>
-          </div>
-          <div className={s.cardContainer}>
-            <Card
-              target="/stupid-tree"
-              header="Idiotic Tree Keeps Trying To Plant Seeds On Sidewalk"
-              body="PORTLAND, OR—Pointing out that the total dipshit had dropped
-                dozens of acorns all along the length of pavement, sources
-                confirmed Thursday that a completely idiotic tree keeps trying
-                to plant seeds on the sidewalk. “Come on, you fucking moron, what
-                are you doing? That’s concrete"/>
-          </div>
-        </div>
-      </div>
-    )
+class HomePage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {ships: 'no ships yet'}
   }
 
+  componentDidMount() {
+    const _this = this
+    setInterval(() => {
+      _this.updateShipState()
+    }, 5000)
+  }
+
+  updateShipState() {
+    let _this = this
+    fetch('http://localhost:9000').then(res => {
+      return res.json()
+    }).then(json => {
+      console.log(json)
+      _this.setState({ships: json})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  render() {
+    // return (<div className={s.ship}></div>)
+    return (<div>{JSON.stringify(this.state.ships)}</div>)
+  }
 }
 
 export default HomePage
